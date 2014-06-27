ed1851edc6fda2539996271fa085a76958f36b9b
Dominik Röttsches <dominik.rottsches@intel.com>
2014-06-27 11:35:09 +0300

    xcode version

--- fallbacktest/main.cpp
+++ fallbacktest/main.cpp
@@ -7,12 +7,27 @@
 //
 
 #include <iostream>
+#include <fontconfig/fontconfig.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    FcInit();
+    FcConfig* config = FcInitLoadConfigAndFonts();
+    FcPattern* pat = FcPatternCreate();
+    FcObjectSet* os = FcObjectSetBuild (FC_FAMILY, FC_STYLE, FC_LANG, FC_FILE, (char *) 0);
+    FcFontSet* fs = FcFontList(config, pat, os);
+    printf("Total matching fonts: %d\n", fs->nfont);
+    for (int i=0; fs && i < fs->nfont; ++i) {
+        FcPattern* font = fs->fonts[i];
+        FcChar8* file, *style, *family;
+        if (FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch &&
+            FcPatternGetString(font, FC_FAMILY, 0, &family) == FcResultMatch &&
+            FcPatternGetString(font, FC_STYLE, 0, &style) == FcResultMatch)
+        {
+            printf("Filename: %s (family %s, style %s)\n", file, family, style);
+        }
+    }
+    if (fs) FcFontSetDestroy(fs);
     return 0;
 }
 
