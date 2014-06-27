bcfee0dc18753b6548759288125831629a7dde9c
Dominik Röttsches <d-r@roettsches.de>
2014-06-27 13:17:48 +0300

    Sorting the list by numSupportedFonts, not removing fonts.

--- fallbacktest/main.cpp
+++ fallbacktest/main.cpp
@@ -1,22 +1,15 @@
-//
-//  main.cpp
-//  fallbacktest
-//
-//  Created by Dominik Rottsches on 27/06/14.
-//  Copyright (c) 2014 Dominik Rottsches. All rights reserved.
-//
-
 #include <iostream>
 #include <fontconfig/fontconfig.h>
 #include <list>
 #include <string.h>
+#include <vector>
 
 
 struct HasCharEntry {
-    HasCharEntry() : numSupportedFonts(0), filename("") {};
+    HasCharEntry() : numSupportedFonts(0), supportedBy(std::vector<std::string>()) {};
     FcChar32 theChar;
     unsigned numSupportedFonts;
-    std::string filename;
+    std::vector<std::string> supportedBy;
 };
 
 typedef std::list<HasCharEntry> HasCharList;
@@ -32,12 +25,16 @@ void fillUcsChars() {
     }
 }
 
+bool compareNumSupported(const HasCharEntry& first, const HasCharEntry& second) {
+    return first.numSupportedFonts < second.numSupportedFonts;
+}
+
 int main(int argc, const char * argv[])
 {
     FcInit();
     FcConfig* config = FcInitLoadConfigAndFonts();
     FcPattern* pat = FcPatternCreate();
-    FcObjectSet* os = FcObjectSetBuild (FC_FAMILY, FC_STYLE, FC_LANG, FC_FILE, (char *) 0);
+    FcObjectSet* os = FcObjectSetBuild (FC_FAMILY, FC_STYLE, FC_LANG, FC_FILE, FC_CHARSET, (char *) 0);
     FcFontSet* fs = FcFontList(config, pat, os);
     printf("Total matching fonts: %d\n", fs->nfont);
     
@@ -46,33 +43,36 @@ int main(int argc, const char * argv[])
     for (int i=0; fs && i < fs->nfont && i < 10; ++i) {
         FcPattern* font = fs->fonts[i];
         FcChar8* file, *style, *family;
-        if (FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch &&
-            FcPatternGetString(font, FC_FAMILY, 0, &family) == FcResultMatch &&
-            FcPatternGetString(font, FC_STYLE, 0, &style) == FcResultMatch)
-        {
-            printf("Filename: %s (family %s, style %s)\n", file, family, style);
-        }
+        if (FcPatternGetString(font, FC_FILE, 0, &file) != FcResultMatch ||
+            FcPatternGetString(font, FC_FAMILY, 0, &family) != FcResultMatch ||
+            FcPatternGetString(font, FC_STYLE, 0, &style) != FcResultMatch)
+            continue;
 
         FcCharSet* charSet;
-        FcPatternGetCharSet(font, FC_CHARSET, 0,  &charSet);
+        if (FcPatternGetCharSet(font, FC_CHARSET, 0,  &charSet) != FcResultMatch) {
+            printf("Font %s cannot report charset.\n", family);
+            continue;
+        }
 
         HasCharList::iterator j = charSupportList.begin();
         while (j != charSupportList.end())
         {
             if (FcCharSetHasChar(charSet, (*j).theChar)) {
                 (*j).numSupportedFonts++;
-                if ((*j).numSupportedFonts > 1) {
-                    printf("Char %x removed, already supported by %s\n", (*j).theChar, (*j).filename.c_str());
-                    charSupportList.erase(j++); // remove and increment
-                    continue;
-                }
-                printf("First font to support %x, adding filename %s.\n", (*j).theChar, file);
-                (*j).filename = std::string((char*)file);
+                (*j).supportedBy.push_back(std::string((char*)file));
+                
             }
             ++j;
         }
         
     }
+    
+    charSupportList.sort(compareNumSupported);
+    for (HasCharList::iterator i = charSupportList.begin(); i != charSupportList.end(); i++) {
+        if ((*i).numSupportedFonts)
+            printf("Char: 0x%x Supported by: %d fonts\n", (*i).theChar, (int)(*i).supportedBy.size());
+    }
+    
     if (fs)
         FcFontSetDestroy(fs);
     return 0;
