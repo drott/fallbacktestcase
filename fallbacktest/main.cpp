666c9900fa5eb6f97cd5a09b40b77ac8803df14e
Dominik Röttsches <dominik.rottsches@intel.com>
2014-06-27 10:42:24 +0300

    Initial Commit

---
+++ fallbacktest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fallbacktest
+//
+//  Created by Dominik Rottsches on 27/06/14.
+//  Copyright (c) 2014 Dominik Rottsches. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
